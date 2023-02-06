0018603772c882b11e1e6f2a62950e9a011bc2f7
tba-adnan <tebbaa.adnane.dev@gmail.com>
2023-02-06 17:01:17 +0100

    Fix.

--- src/main/kotlin/filters.kt
+++ src/main/kotlin/filters.kt
@@ -1,6 +1,12 @@
 fun main() {
     println("Main start.")
+    demo()
 
 }
 
-val list = listOf("adnane", "haitam","amjad", "demo_three")
+val list = listOf("adnane", "haitam","amjad", "xc020")
+fun demo() {
+    println(list)
+    var filter = (list.filter { it[2] == 'j' })
+    println("the name is $filter")
+}
