330b72d396241600bc49663747fc10cf16398a83
tba-adnan <tebbaa.adnane.dev@gmail.com>
2023-02-07 15:29:57 +0100

    Fix.

---
+++ src/main/kotlin/exercise.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    print("Enter the number of stars in the base: ")
+    val stars = readln().toInt()
+
+
+
+
+    for (star in 1..stars  ) {
+      print("*")
+    }
+}
