de326eee30c021e86c9dfa9cf94c6534fbe68eed
tba-adnan <tebbaa.adnane.dev@gmail.com>
2023-02-09 11:22:30 +0100

    fix console Design.

--- Kotlin-Basics/Kotlin-Exercises/Exercise-2.kt
+++ Kotlin-Basics/Kotlin-Exercises/Exercise-2.kt
@@ -1,8 +1,43 @@
+val mylist = arrayListOf<String>("demo0","demo1","demo2")
+
 fun main() {
- crud()
+    println("""
+                                 ___ 
+  _____ _____ _ _ __(_)___ ___  |_  )
+ / -_) \ / -_) '_/ _| (_-</ -_)  / / 
+ \___/_\_\___|_| \__|_/__/\___| /___|
+                          par Adnane Tebbaa.           
+""".trimIndent())
+    println("")
+    print("select display, add, delete (di/ad/de) > ")
+    val user = readln()
+    if (user == "di") {
+        display()
+    }
+    if (user == "de") {
+        print("delete > ")
+        val de_element = readln()
+        delete(de_element)
+    }
+    if (user == "ad") {
+        print("add element > ")
+        val ad_element = readln()
+        add(ad_element)
+    }
+
 }
 
-fun crud() {
-    val mylist = listOf("adanen","demo")
-    println(mylist)
+
+fun display() {
+    println("current list : $mylist")
+}
+
+fun delete(input: String) {
+    mylist.remove("$input")
+    println("current list: $mylist")
+}
+
+fun add(add: String) {
+    mylist.add("$add")
+    println("current list : $mylist")
 }
