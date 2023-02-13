61fa80c1e52b679bfb4a8c9e6ffd9b4d5c4811a3
tba-adnan <tebbaa.adnane.dev@gmail.com>
2023-02-13 16:48:15 +0100

    OOP fix.

---
+++ Kotlin-OOP/src/main/kotlin/Computer.kt
@@ -0,0 +1,4 @@
+class Computer(val height: Number, val width: Number) {
+
+
+}
--- Kotlin-OOP/src/main/kotlin/main.kt
+++ Kotlin-OOP/src/main/kotlin/main.kt
@@ -1,10 +1,12 @@
 import School
 import Classroom
 import Monitor
+import Computer
 fun main() {
     val initial = School()
     val classroom = Classroom("adnane", 18)
     val monitor = Monitor(100, 70)
+    val computer = Computer(13)
     initial.main()
     initial.room(3)
     classroom.main()
