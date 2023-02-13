1cae44f611641c47ae6e12a687e2aed3a746295d
tba-adnan <tebbaa.adnane.dev@gmail.com>
2023-02-13 09:59:24 +0100

    File organisation.

--- Kotlin-Exercises/Exercise-1.kt
+++
@@ -1,7 +0,0 @@
-fun main() {
-    println("Enter n of lines : ")
-    val rows = readln().toInt()
-    for (star in 1..rows) {
-        println(" ".repeat(rows - star) + "*".repeat(2 * star - 1))
-    }
-}
