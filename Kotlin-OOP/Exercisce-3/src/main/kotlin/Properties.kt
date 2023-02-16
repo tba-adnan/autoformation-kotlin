6d714ad3c99ccb8ecde492ed26259f40f69deeac
tba-adnan <tebbaa.adnane.dev@gmail.com>
2023-02-16 13:05:48 +0100

    fix.

--- Kotlin-OOP/Exercisce-3/src/main/kotlin/Properties.kt
+++ Kotlin-OOP/Exercisce-3/src/main/kotlin/Properties.kt
@@ -1,6 +1,16 @@
 import Operations
-
-class Properties {
-
+abstract class Properties: Operations {
+    val mylist = arrayListOf<String>("produit0","produit1","produit2")
+    override fun afficher() {
+        println("[>] current list: $mylist")
+    }
+    override fun ajouter() {
+        mylist.add("demox")
+        println("[>] current list: $mylist")
+    }
+    override fun supprimer() {
+        mylist.remove("demox")
+        println("[>] current list: $mylist")
+    }
 
 }
