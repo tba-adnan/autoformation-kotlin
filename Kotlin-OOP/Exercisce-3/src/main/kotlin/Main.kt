f61aa47128c400d004312aa9375d2e8d84c10dd3
tba-adnan <tebbaa.adnane.dev@gmail.com>
2023-02-17 10:40:19 +0100

    File fix.

--- Kotlin-OOP/Exercisce-3/src/main/kotlin/Main.kt
+++ Kotlin-OOP/Exercisce-3/src/main/kotlin/Main.kt
@@ -1,4 +1,4 @@
-fun main() {
+fun banner() {
     println("running...").also(::println)
     println("\n" +
             "  ___                _        ____\n" +
@@ -7,6 +7,81 @@ fun main() {
             " |___/_\\_\\_| \\___\\__|_\\__\\___|___/\n" +
             "                                  \n Gestion des Produits, Réaliser Par adnane Tebbaa")
 
+}
 
+fun main() {
+    banner()
+    val productManager: ProductManager = ProductManager()
 
+    while (true) {
+        print("\nPlease choose an option - `help` for more information: ")
+        val args: List<String>? = readLine()?.split(" ")
+
+        if (args == null) {
+            println("No option found")
+            continue
+        }
+
+        when (args[0]) {
+            "list" -> productManager.listProducts()
+            "add" -> productManager.add(args[1])
+            "edit" -> productManager.edit(args[1], args[2])
+            "del" -> productManager.delete(args[1])
+            "help" -> help()
+            "exit" -> break
+            else -> println("No option found")
+        }
+    }
+}
+
+class ProductManager {
+    private val products: MutableList<Product> = mutableListOf()
+
+    fun listProducts() {
+        if (this.products.isEmpty()) {
+            println("No products found.")
+            return
+        }
+
+        println(this.products.joinToString { it.name })
+    }
+
+    fun add(name: String) {
+        if (this.products.find { it.name == name } != null) {
+            println("Product already exists.")
+            return
+        }
+
+        this.products.add(
+            Product(name)
+        )
+        println("Product added successfully.")
+    }
+
+    fun edit(name: String, newName: String) {
+        val elemIndex: Int = this.products.indexOfFirst {it.name == name}
+        this.products[elemIndex].name = newName
+        println("Product updated successfully.")
+    }
+
+    fun delete(name: String) {
+        val elemIndex: Int = this.products.indexOfFirst { it.name == name }
+        this.products.removeAt(elemIndex)
+        println("Product deleted successfully.")
+    }
+}
+
+data class Product(var name: String)
+
+val helpText: String = """
+        You can use this options:
+        -list: List all products.
+        -add {name}: To create a new product.
+        -edit {name} {new_name}: To update a product.
+        -del {name}: To delete a product.
+        -exit: Exit the program.
+    """.trimIndent()
+
+fun help() {
+    println(helpText)
 }
