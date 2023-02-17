6ef06c72e58944a002dedbed9f23d53282599f35
tba-adnan <tebbaa.adnane.dev@gmail.com>
2023-02-17 15:28:18 +0100

    Dice Roller.

--- Android/HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ Android/HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.happybirthday
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
+            toast.show()
+            // val maintext: TextView = findViewById(R.id.)
+            val diceImage: ImageView = findViewById(R.id.imageView)
+
+        }
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val resultTextView: TextView = findViewById(R.id.imageView)
+        resultTextView.text = diceRoll.toString()
+    }
+}
+
+class Dice(val numSides: Int) {
+    fun roll(): Int {
+        return (1..numSides).random()
     }
 }
