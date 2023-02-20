265c40199d4af5ea93adaa95e1487ae191986785
tba-adnan <tebbaa.adnane.dev@gmail.com>
2023-02-20 09:57:20 +0100

    adding DiceRoller.

--- Android/DiceRoller/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ Android/DiceRoller/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -1,4 +1,5 @@
 package com.example.happybirthday
+
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
@@ -15,25 +16,21 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener {
             val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
             toast.show()
+            // val maintext: TextView = findViewById(R.id.)
             val diceImage: ImageView = findViewById(R.id.imageView)
+
         }
         rollButton.setOnClickListener {
             rollDice()
         }
+
     }
 
     private fun rollDice() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
         val diceImage: ImageView = findViewById(R.id.imageView)
-        when (diceRoll) {
-            1 -> diceImage.setImageResource(R.drawable.dice_1)
-            2 -> diceImage.setImageResource(R.drawable.dice_2)
-            3 -> diceImage.setImageResource(R.drawable.dice_3)
-            4 -> diceImage.setImageResource(R.drawable.dice_4)
-            5 -> diceImage.setImageResource(R.drawable.dice_5)
-            6 -> diceImage.setImageResource(R.drawable.dice_6)
-        }
+        diceImage.setImageResource(R.drawable.dice_2)
     }
 }
 
