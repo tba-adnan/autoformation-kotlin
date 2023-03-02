71c20766de37d22cea8d1df10fc66c9a72730ea4
tba-adnan <tebbaa.adnane.dev@gmail.com>
2023-03-02 11:09:49 +0100

    Lesson 5: RecycleView ✔️

--- Android/Recycleview/app/src/main/java/com/example/recycleview/MainActivity.kt
+++ Android/Recycleview/app/src/main/java/com/example/recycleview/MainActivity.kt
@@ -2,15 +2,19 @@ package com.example.recycleview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.RecyclerView
 import android.widget.TextView
+import com.example.recycleview.adapter.ItemAdapter
 import com.example.recycleview.data.Datasource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val textView: TextView = findViewById(R.id.textview)
-        textView.text = Datasource().loadAffirmations().size.toString()
 
+        val myDataset = Datasource().loadAffirmations()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+        recyclerView.setHasFixedSize(true)
     }
 }
