f0a6ea9253e9f232e1368b2dc19766e3749b8505
TebbaaX <>
2023-05-02 22:42:49 +0100

    Added Data Classes.

---
+++ Kotlin-vRecap/Classes/src/main/kotlin/Data.kt
@@ -0,0 +1,2 @@
+data class Data(val name: String, val nickname: String, val age: Int ) {
+}
--- Kotlin-vRecap/Classes/src/main/kotlin/Main.kt
+++ Kotlin-vRecap/Classes/src/main/kotlin/Main.kt
@@ -2,6 +2,8 @@ fun main() {
     println("Hello World!")
     val Classic = ClassOne()
     val Construct = Const("adnane")
+    val DataL = Data("adnane", nickname = "tebbaa", age = 18 )
+
     //
     Classic.Name()
     Classic.absmain()
